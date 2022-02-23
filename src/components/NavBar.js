0a53e96ca28e23a94a880dd650155948cdf3a7d7
rocio cacciamano <rociocacciamano1511@gmail.com>
2022-02-23 11:17:57 -0300

    desafio clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+        <>
+          <nav id="nav">
+            <h1>E-COMMERCE</h1>
+            <ul>
+              <li><a href="#">Categorias</a></li>
+              <li><a href="#">Mis compras</a></li>
+              <li><a href="#">Contacto</a></li>
+            </ul>
+          </nav>
+        </>
+    )
+}
+
+export default NavBar
